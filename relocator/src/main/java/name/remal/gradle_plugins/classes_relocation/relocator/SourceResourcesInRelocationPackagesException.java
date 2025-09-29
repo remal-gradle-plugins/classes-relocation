ed48ac6f8f6b76fff83c9da0a94edb47b1576b4d
Semyon Levin <levin.semen@gmail.com>
2025-09-29 12:48:15 +0400

    fix build

--- relocator/src/main/java/name/remal/gradle_plugins/classes_relocation/relocator/SourceResourcesInRelocationPackagesException.java
+++ relocator/src/main/java/name/remal/gradle_plugins/classes_relocation/relocator/SourceResourcesInRelocationPackagesException.java
@@ -4,12 +4,13 @@ import java.util.Collection;
 
 public class SourceResourcesInRelocationPackagesException extends ClassesRelocationException {
 
+    @SuppressWarnings("java:S3024")
     private static String createMessage(Collection<String> sourcePackageNamesToRelocate) {
         var sb = new StringBuilder();
         sb.append("Source JAR contains packages for relocation:");
         sourcePackageNamesToRelocate.forEach(resource -> sb.append("\n  ").append(resource));
-        sb.append("\n\nIf you want to redefine resources for relocation")
-            .append(", create a separate dependency and relocate it.\n");
+        sb.append("\n\nIf you want to redefine resources for relocation"
+            + ", create a separate dependency and relocate it.\n");
         return sb.toString();
     }
 
